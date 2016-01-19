1e8b408534b862bddd4a837f45d2adcde6bdec93
Peter Lauri <peterlauri@gmail.com>
2016-01-19 01:27:34 +0100

    set browser to be phantomjs

--- test_splinter.py
+++ test_splinter.py
@@ -1,9 +1,9 @@
 import pytest
 
 
-# @pytest.fixture(scope='session')
-# def splinter_webdriver():
-#     return 'phantomjs'
+@pytest.fixture(scope='session')
+def splinter_webdriver():
+    return 'phantomjs'
 
 def test_visit_google(browser):
     browser.visit('http://www.google.com')
