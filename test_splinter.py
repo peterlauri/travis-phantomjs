6c79f2820cd0db89c3dc5bef04946674d28896b0
Peter Lauri <peterlauri@gmail.com>
2016-01-19 01:22:42 +0100

    initial travis and tests

---
+++ test_splinter.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+# @pytest.fixture(scope='session')
+# def splinter_webdriver():
+#     return 'phantomjs'
+
+def test_visit_google(browser):
+    browser.visit('http://www.google.com')
